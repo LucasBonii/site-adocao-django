024f08121d57cc8255bd60fdf745db514fafa7f6
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2025-06-30 20:52:10 -0300

    first pages, initializing frontend

--- backend_api/core/admin.py
+++ backend_api/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import  Ong, Animal, TutorAnimal, Usuario, Candidatura
+
+admin.site.register(Ong)
+admin.site.register(Animal)
+admin.site.register(TutorAnimal)
+admin.site.register(Usuario)
+admin.site.register(Candidatura)
--- backend_api/core/models.py
+++ backend_api/core/models.py
@@ -12,10 +12,16 @@ class Usuario(AbstractUser):
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.username} - {self.tipo}'
+
 class Ong(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
     cnpj = models.CharField(max_length=20)
     descricao = models.TextField()
+
+    def __str__(self):
+        return self.descricao
 
 class Animal(models.Model):
     nome = models.CharField(max_length=100)
@@ -26,6 +32,9 @@ class Animal(models.Model):
     descricao = models.TextField()
     status = models.CharField(max_length=50)
     ong = models.ForeignKey(Ong, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.nome} - {self.especie} - {self.ong} || {self.status}'
 
 class Candidatura(models.Model):
     adotante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
@@ -39,3 +48,6 @@ class TutorAnimal(models.Model):
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     data_inicio_responsabilidade = models.DateField()
     observacoes = models.TextField()
+
+    def __str__(self):
+        return f'{self.animal} - {self.tutor}'
